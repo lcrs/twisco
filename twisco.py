9d021cd65ad0e21932e99f1be251938f40e152b4
Lewis Saunders <lewis.saunders@gmail.com>
2015-06-15 22:50:47 +0100

    Getting favs of friends works, immediately hits rate limit

--- twisco.py
+++ twisco.py
@@ -1,4 +1,5 @@
-import os, oauth2
+import os, oauth2,json
+
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 ACCESS_KEY = os.environ['ACCESS_KEY']
@@ -6,6 +7,17 @@ ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
 consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 token = oauth2.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
-client = oauth2.Client(consumer, token)
 
-print client.request('https://api.twitter.com/1.1/statuses/home_timeline.json')
+def req(url):
+	client =oauth2.Client(consumer, token)
+	(response, content) = client.request(url)
+	return json.loads(content)
+
+acct = req('https://api.twitter.com/1.1/account/verify_credentials.json')
+
+friends = req('https://api.twitter.com/1.1/friends/ids.json?user_id=%s' % acct['id'])
+
+for friend in friends['ids'][0:1]:
+	favs = req('https://api.twitter.com/1.1/favorites/list.json?user_id=%s' % friend)
+	for fav in favs:
+		print fav['text'] + '\n\n'
