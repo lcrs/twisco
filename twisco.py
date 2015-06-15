501450d5287ff5334d6c5840d83c586f4ecabc3b
Lewis Saunders <lewis.saunders@gmail.com>
2015-06-15 22:16:59 +0100

    working timeline get

--- twisco.py
+++ twisco.py
@@ -1,9 +1,11 @@
-import oauth2
+import os, oauth2
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_KEY = os.environ['ACCESS_KEY']
+ACCESS_SECRET = os.environ['ACCESS_SECRET']
 
-def oauth_req(url, key, secret, http_method="GET", post_body=None, http_headers=None):
-    consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    token = oauth.Token(key=key, secret=secret)
-    client = oauth.Client(consumer, token)
-    resp, content = client.request( url, method=http_method, body=post_body, headers=http_headers, force_auth_header=True )
+consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+token = oauth2.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+client = oauth2.Client(consumer, token)
 
-    return content home_timeline = oauth_req( 'https://api.twitter.com/1.1/statuses/home_timeline.json', 'abcdefg', 'hijklmnop' )
+print client.request('https://api.twitter.com/1.1/statuses/home_timeline.json')
