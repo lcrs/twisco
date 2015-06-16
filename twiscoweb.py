6132374ede9ebebf4829adcffbcbea66d9f9b34b
Lewis Saunders <lewis.saunders@gmail.com>
2015-06-17 00:04:37 +0100

    single column, moves to seen folder

--- twiscoweb.py
+++ twiscoweb.py
@@ -1,14 +1,15 @@
 # Render tweets from twiscod.py's tweets folder into a webpage
 # lewis@lewissaunders.com
 # TODO:
-#	o balance amongst friends or random overall?
-#	o delete as we render, or soft delete by moving?
+#	o balance amongst friends?
 
 import os, glob, random
 
-print """<script>window.twttr = (function(d, s, id) {
+print """Content-Type: text/html
+
+<script>window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
+	t = window.twttr || {};
   if (d.getElementById(id)) return t;
   js = d.createElement(s);
   js.id = id;
@@ -17,32 +18,35 @@ print """<script>window.twttr = (function(d, s, id) {
  
   t._e = [];
   t.ready = function(f) {
-    t._e.push(f);
+	t._e.push(f);
   };
  
   return t;
-}(document, "script", "twitter-wjs"));</script>
-<style>
-td {
-	vertical-align: top;
-}
-</style>
-<table><tr><td>"""
+}(document, "script", "twitter-wjs"));</script>"""
 
-favs = []
-friends = glob.glob('tweets/*')
-for friend in friends:
-	tweets = glob.glob(friend + '/*')
-	for tweet in tweets:
-		f = open(tweet)
-		favs.append(f.read())
-		f.close()
+tweets = glob.glob('tweets/*/*')
 
-for t in range(min(len(favs), 50)):
-	randomindex = random.randint(0, len(favs)-1)
-	if(t % 10 == 0):
-		print '</td><td>'
-	print favs[randomindex]
-	favs.pop(randomindex)
+shown = 0
+while(shown < min(len(tweets), 20)):
+	r = random.randint(0, len(tweets) - 1)
+	tweet = tweets[r]
+	(friend, twid) = tweet.split('/')[1:]
+	seenpath = 'seen/%s/%s' % (friend, twid)
+	if(os.path.exists(seenpath)):
+		os.rename(tweet, seenpath)
+		continue
+	
+	f = open(tweet)
+	html = f.read()
+	f.close()
 
-print '</td></tr></table>'
+	print html.replace('twitter-tweet', 'twitter-tweet tw-align-center') + '<br><br>'
+
+	try:
+		os.makedirs('seen/%s' % friend)
+	except:
+		pass
+	os.rename(tweet, seenpath)
+	tweets.pop(r)
+
+	shown = shown + 1
